1681cebe16986c522056932c9ec28b8e41eaff36
silvaguedes <aldairguedessilvao2gmail.com>
2024-11-11 17:17:08 -0300

    adicionado arquivo de url

--- services/api.js
+++ services/api.js
@@ -1,13 +1,7 @@
-
-
-// Importa a instância do axios
- 
-
-
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:'http://localhost:3000'
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+});
 
-})
-export default api
+export default api;
